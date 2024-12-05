d6266b1b662204f9e7525f46a7b01ab87ff69d3c
NeptuneLuke <luca1cortinovis@gmail.com>
2024-12-05 21:44:38 +0100

    Adding a gradient color to the pixels

--- frag_shader.glsl
+++ frag_shader.glsl
@@ -1,5 +1,6 @@
 #version 450
 
+in vec4 varying_colors;
 out vec4 color;
 
 // Here the following uniform are not used,
@@ -8,5 +9,11 @@ uniform mat4 modelview_matrix;
 uniform mat4 projection_matrix;
 
 void main() { 
-	color = vec4(1.0, 0.0, 0.0, 1.0); // hardcoded red color for every cube pixel
+	//color = vec4(1.0, 0.0, 0.0, 1.0); // hardcoded red color for every cube's pixel
+	color = varying_colors;
 }
+
+/*
+ * in -> the elements will receives values from the buffer
+ * out -> the elements will send values to the next stage in the pipeline
+*/
--- vert_shader.glsl
+++ vert_shader.glsl
@@ -1,12 +1,19 @@
 #version 450
 
+layout (location=0) in vec3 position;
+
 uniform mat4 modelview_matrix;
 uniform mat4 projection_matrix;
 
-layout (location=0) in vec3 position;
+out vec4 varying_colors; // color gradient based on the pixels location
+// when talking about programmer-defined interpolated vertex attributes
+// the naming convetion of the variables include the word "varying"
 
 void main() { 
 	gl_Position = projection_matrix * modelview_matrix * vec4(position, 1.0);
+	varying_colors = vec4(position, 1.0) * 0.5 + vec4(0.5, 0.5, 0.5, 0.5);
+	// multiplying the location by 0.5 and adding 0.5 converts the range of values
+	// from [-1,1] to[0,1]
 }
 // The multiplication applies the matrix transforms to the vertex, converting it to
 // camera space. Those values are put in the built-in OpenGL output variable gl_Position
